4bd8c671cc451b71337bd51c7ac365d0e03db346
romain caillard <mttdiamond@gmail.com>
2023-06-05 16:54:11 +0200

    model parking

---
+++ db/migrate/20230605145338_create_parkings.rb
@@ -0,0 +1,11 @@
+class CreateParkings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :parkings do |t|
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
