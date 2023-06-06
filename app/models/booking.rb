c2d23aade9b6864e6b7acd4b8638c5db5f636516
magali mares <maga.mares@gmail.com>
2023-06-06 16:31:17 +0200

    create booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :giver_car, class_name: "Car"
   belongs_to :taker_car, class_name: "Car", optional: true
   belongs_to :parking
+
+  validates :available_at, presence: true
 end
--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,4 +2,5 @@ class Parking < ApplicationRecord
   has_many :bookings, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :address, presence: true
 end
