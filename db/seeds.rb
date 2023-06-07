988453db42de171d451a20c45699edc2fddbdd49
magali mares <maga.mares@gmail.com>
2023-06-07 15:17:39 +0200

    cards en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ aligiver = User.create!(first_name: "ali", last_name: "Ouzidhouh", email: "ali@g
 damsogiver = User.create!(first_name: "damso", last_name: "Rappe", email: "damso@gmail.com", password: "azerty")
 
 
-car_romaintaker = Car.create!(car_model: "Renault Twizy", plate: "AA-567-EB", car_color: "Mauve", car_type: "compact", user: romaintaker)
+car_romaintaker = Car.create!(car_model: "Renault Twizy", plate: "AA-567-EB", car_color: "Mauve", car_type: "citadine", user: romaintaker)
 car_karengiver = Car.create!(car_model: "Renault Clio", plate: "AA-123-BB", car_color: "Rouge", car_type: "citadine", user: karengiver)
 car_magalietaker = Car.create!(car_model: "Renault Zoé", plate: "AB-456-CD", car_color: "Bleu", car_type: "citadine", user: magalietaker)
 car_aligiver = Car.create!(car_model: "Renault Scénic", plate: "AZ-678-ER", car_color: "Jaune", car_type: "berline", user: aligiver)
@@ -28,6 +28,6 @@ parking_3 = Parking.create!(address: "63, boulevard Voltaire, 75011 PARIS", lati
 
 booking_1 = Booking.create!(available_at: "20230606130100", parking: parking_1, giver_car: car_karengiver )
 booking_2 = Booking.create!(available_at: "20230606132300", parking: parking_2, giver_car: car_aligiver )
-booking_3 = Booking.create!(available_at: "20230606133300", parking: parking_3, giver_car: car_damsogiver )
+booking_3 = Booking.create!(available_at: "20230606133300", parking: parking_3, giver_car: car_damsogiver, taker_car: car_romaintaker )
 
 puts "seeds upload all"
