97cd549f7ce0a9fe81ce0a89f544ab5acb066a32
romain caillard <mttdiamond@gmail.com>
2023-06-14 10:58:50 +0200

    css-navbar-step2

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -54,6 +54,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: { top: 0, bottom: 300, left: 70, right: 70 }, maxZoom: 15, duration: 0 })
   }
 }
